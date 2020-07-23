464a307465f1a6734ebedfa2274368a9b7c0def2
Extlros <67964566+Extlros@users.noreply.github.com>
2020-07-23 09:33:36 -0500

    Add files via upload

--- CNT_obj.cpp
+++ CNT_obj.cpp
@@ -1,4 +1,6 @@
 #include "CNT_obj.h"
+#include <fstream>
+#include <cstdlib>
 
 	// CNT_ode4 is an implemenetation of the 4th order runge kutta method. The right hand side (frhs)
 	// stores the details of the forces in the equation of motion, this is a separate equation called below.	
@@ -76,16 +78,18 @@
 	{
 	    double d0,d1,d2,d3,d01,d11,d22,d32,d12,temp,rr,rr1,rr2,d[N_cnt/6],Del[N_cnt],a;
 	    int m,i,n;
+	    
 	    //measure some lengths
-	    for(n=0;n<N_cnt/6-1;n++){
+	    for(n=0;n<N_cnt/6-1;n++)
+	    {
 	        d[n]=0;
-			
-	        for(i=0;i<3;i++){
+	        for(i=0;i<3;i++)
+		{
 	            temp=y[(n+1)*6+i]-y[n*6+i];
 	            Del[n*6+i]=temp;
 	            temp*=temp;
 	            d[n]+=temp;
-			}
+		}
 	        d[n]=sqrt(d[n]);
 	    }
 	    //perform some dot-products
@@ -110,13 +114,17 @@
 	        d22=d2*d2;
 	        d3=d[n/6+1];
 	        d32=d3*d2;
-	        // dVx/dt = -gamma*Vx + F_Vx(t) ... dVz/dt = -gamma*Vz + F_Vz(t)
-			for(i=0;i<3;i++){
+	        // dVx/dt = -gamma*Vx + F_Vx(t) ... dVz/dt = -gamma*Vz + F_Vz(t)    
+			
+	    	for(i=0;i<3;i++){
 	            m=n+i;
-	            f[m+3]=-gam_cnt*y[m+3]+ //damping forces
-						K_cnt*(Del[m]*(1-X0_cnt/d2)-Del[m-6]*(1-X0_cnt/d1))+ //forces due to stretching
-						kap_cnt*((Del[m-12]-Del[m-6]*rr/d11)/d01-(Del[m+6]-Del[m]*rr2/d22)/d32+(Del[m]*(1+rr1/d22)-Del[m-6]*(1+rr1/d11))/d12);//forces due to bending
-	        }
+	            f[m+3]=(-gam_cnt[n/6]*y[m+3])+ //damping forces
+			     (K_cnt_array[n/6]/massarray[n/6])*(Del[m]*(1-X0_cnt/d2)-Del[m-6]*(1-X0_cnt/d1))+ //forces due to stretching
+		    	 (kap_cnt_array[n/6-1]/massarray[n/6])*(Del[m-12]-Del[m-6]*rr/d11)/d01-
+			     (kap_cnt_array[n/6+1]/massarray[n/6])*(Del[m+6]-Del[m]*rr2/d22)/d32+
+			     (kap_cnt_array[n/6]/massarray[n/6])*(Del[m]*(1+rr1/d22)-Del[m-6]*(1+rr1/d11))/d12;//forces due to bending
+			
+			}
 	        //shift stored values for reuse on next data point
 			rr=rr1;
 	        rr1=rr2;
@@ -160,50 +168,70 @@
 		if ((temp=AM*iy)>RNMX) return RNMX;
 		else return temp;
 	}
-		
+
 	//performs one step of numerical integration with runge kutta
 	void CNT_obj::timestep( double t_in)
 	{	
 		//implements thermal noise as velocity perturbations
 		for(i=15;i<N_cnt-12;i+=6)
             {
-                y_cnt[i]+=sig_cnt*(2*ran1(seed)-1);
-                y_cnt[i+1]+=sig_cnt*(2*ran1(seed)-1);
-                y_cnt[i+2]+=sig_cnt*(2*ran1(seed)-1);
+            	n=(i-3)/6;
+                y_cnt[i]+=sig_cnt[n]*(2*ran1(seed)-1);
+                y_cnt[i+1]+=sig_cnt[n]*(2*ran1(seed)-1);
+                y_cnt[i+2]+=sig_cnt[n]*(2*ran1(seed)-1);
 			}
 		//numerical inegration performed	
 		(this->CNT_ode4)(y_cnt, h_cnt, t_in, &CNT_obj::CNT_myrhs_wF);
 	
 	}
+
 	//performs one step of numerical integration with Stoermers method
 	void CNT_obj::timestep_stoermer( double t_in,int nstep)
 	{
 			 (this->CNT_stoerm)(y_cnt, h_cnt, t_in, nstep , &CNT_obj::CNT_myrhs_wF);	
 	}
+
 	//update a constant force applied
 	void CNT_obj::set_F(double F_in)
 	{
 		F_cnt=F_in;
 		Fper_cnt=F_in/((double)Npoints-3.0);
 	}
+
 	//changes the dissipation rate
 	void CNT_obj::set_gam(double gam_in)
 	{
-		gam_cnt=gam_in;
+		double mass_sum, mass_average;
+		mass_sum=0;
+		
+		for(int i=0;i<N_cnt/6;i++)
+		{
+			mass_sum=mass_sum+massarray[i];
+		}
+	
+		mass_average=mass_sum/(N_cnt/6);
+		
+		for(int i=0;i<N_cnt/6;i++)
+		{
+			gam_cnt[i]=gam_in*(mass_average/massarray[i]);
+		}
 		update_sig();
 	}
+
 	//changes the discretized unit of time
 	void CNT_obj::set_h(double h_in)
 	{
 		h_cnt=h_in;
 		update_sig();
 	}
+
 	//sets the temperature
 	void CNT_obj::set_temp(double temp_in)
 	{
 		temp_cnt=temp_in;
 		update_sig();
 	}
+
 	//changes the CNT diameter
 	void CNT_obj::set_d(double d_in)
 	{
@@ -211,24 +239,30 @@
 		 update_Ks();
 		 update_sig();
 	}
+	
 	//changes "velocity perturbation amplitude" based on the simulation parameters
 	void CNT_obj::update_sig() 
 	{
-		sig_cnt=sqrt(h_cnt*temp_cnt/sigma*kb/Pi/d_cnt/X0_cnt)*sqrt(gam_cnt*6.0);
+		for(i=0;i<N_cnt/6;i++)
+		{
+			sig_cnt[i]=sqrt((6*gam_cnt[i]*kb*temp_cnt*h_cnt)/(massarray[i]));
+		}
 	}
+
 	//update spring constants based on hollow CNT model 
 	void CNT_obj::update_Ks()
 	{
+		
 		K_cnt=446/X0_cnt/X0_cnt;
 		kap_cnt=K_cnt*d_cnt*d_cnt/8.0;
 	}
+
 	//returns the "velocity perturbation amplitude"
-	double CNT_obj::get_sig()
+	double* CNT_obj::get_sig()
  	{
   	 	   return sig_cnt;
  	}
- 	
- 	
+	
 	/*-------------------------------------------------------*/
 	//class definition of CNT_obj that stores relevant simulation parameters and coordinates of internal particsls
 	
@@ -241,17 +275,71 @@
 		k3_cnt=k2_cnt+N_cnt;
 		k4_cnt=k3_cnt+N_cnt;
 		tempO_cnt=k4_cnt+N_cnt;
-		cout<<"test in CNT_obj"<<endl;
-		X0_cnt=L_in/((double)Npoints_in-3.0);   
-
-		set_d(d_in);
+		
+		X0_cnt=(L_in)/((double)Npoints_in-3.0);   
+		massarray=new double[Npoints_in];
+		K_cnt_array=new double[Npoints_in];
+		kap_cnt_array=new double[Npoints_in];
+		gam_cnt = new double[Npoints_in];
 		y_cnt=new double[N_cnt];
+		sig_cnt =new double[Npoints_in];
+		//cout<<"test in CNT_obj"<<endl;
+		
+		set_d(d_in);
 		for(int i=0;i<N_cnt;i++) y_cnt[i]=0;
 		for(int i=0;i<N_cnt;i+=6) y_cnt[i]=i/6*X0_cnt;
 		void CNT_myrhs(double [], double, double []);
 		void CNT_ode4(double [], double, double, void(double [], double, double[]));
 		void CNT_stoerm(double [], double, double, int, void(double [], double, double[]));
 	}
+
+double* CNT_obj::massRead(char filename[],long long Nmass)
+{
+	ifstream massfile(filename, ios::in);
+	massfile.seekg(0,ios::beg);
+	double sizemass=double(-massfile.tellg());
+	massfile.seekg(0,ios::end);
+	sizemass+=double(massfile.tellg());
+	char* h=(char*)malloc((int)sizemass);
+	massfile.seekg(0,ios::beg);
+	massfile.read(h,sizemass);
+	double* x=(double*)h;
+	Nmass=(int)sizemass/8;
+	massfile.close();
+	return x;
+}
+
+double* CNT_obj::K_cnt_read(char filename[], long long N_K_cnt)
+{
+	ifstream K_cnt_file(filename,ios::in);
+	K_cnt_file.seekg(0,ios::beg);
+	double size_K_cnt=double(-K_cnt_file.tellg());
+	K_cnt_file.seekg(0,ios::end);
+	size_K_cnt+=double(K_cnt_file.tellg());
+	char* h=(char*)malloc((int)size_K_cnt);
+	K_cnt_file.seekg(0,ios::beg);
+	K_cnt_file.read(h,size_K_cnt);
+	double* x=(double*)h;
+	N_K_cnt=(int)size_K_cnt/8;
+	K_cnt_file.close();
+	return x;
+}
+
+double* CNT_obj::kap_cnt_read(char filename[], long long N_kap_cnt)
+{
+	ifstream kap_cnt_file(filename,ios::in);
+	kap_cnt_file.seekg(0,ios::beg);
+	double size_kap_cnt=double(-kap_cnt_file.tellg());
+	kap_cnt_file.seekg(0,ios::end);
+	size_kap_cnt+=double(kap_cnt_file.tellg());
+	char* h=(char*)malloc((int)size_kap_cnt);
+	kap_cnt_file.seekg(0,ios::beg);
+	kap_cnt_file.read(h,size_kap_cnt);
+	double* x=(double*)h;
+	N_kap_cnt=(int)size_kap_cnt/8;
+	kap_cnt_file.close();
+	return x;
+}
 
 	
 
